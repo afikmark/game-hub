d58c64fe2049e8d03998239218074f2c42836328
afikm <afikmark@gmail.com>
2024-05-07 20:57:17 +0300

    vite api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const VITE_API_KEY  = process.env.REACT_APP_API_KEY;
 
-if (!API_KEY) {
+if (!VITE_API_KEY ) {
     throw new Error("API key is not provided in environment variables");
 }
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: API_KEY
+        key: VITE_API_KEY 
     }
 });
 
